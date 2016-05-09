9156dd8dacb8f4141d41e34b3d776e2b0047fb44
man09004 <man09004@engr.uconn.edu>
2016-05-09 17:00:15 -0400

    Some updates, .project, example

---
+++ dge-em/dge-em.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -cp "target/dge-em-1.0.jar:target/dependency/isoform-common-1.0.jar:target/dependency/jopt-simple-5.0.1.jar:target/dependency/scala-library-2.11.8.jar" edu.uconn.engr.dna.dgeem.DGEStartup $*
+
